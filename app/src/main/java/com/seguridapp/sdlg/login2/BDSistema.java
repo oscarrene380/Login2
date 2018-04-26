3036883d379b926a59b0a4aa025e0551c244a7ce
Alexander9616 <33166184+Alexander9616@users.noreply.github.com>
2018-04-26 09:01:48 -0600

    * Base de datos para Login

---
+++ app/src/main/java/com/seguridapp/sdlg/login2/BDSistema.java
@@ -0,0 +1,37 @@
+package com.seguridapp.sdlg.login2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Don Jose on 26/04/2018.
+ */
+
+public class BDSistema extends SQLiteOpenHelper {
+
+    String sqlCrear = "CREATE TABLE tblUsuarios(idUsuario INTEGER PRIMARY KEY AUTOINCREMENT, Email TEXT, Password TEXT)";
+    String sqlCrear1 = "CREATE TABLE tblUsuarios(idUsuario INTEGER PRIMARY KEY AUTOINCREMENT, Email TEXT, Password TEXT, Alias TEXT)";
+
+    public BDSistema(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //Si no existe la BD, crea la BD, la crea y ejecuta los siguientes comandos.
+        db.execSQL(sqlCrear);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        //Se elimina la versión anterior de la BD
+        db.execSQL("DROP TABLE IF EXISTS tblUsuarios");
+
+        //Se crea la nueva versión de la tabla
+        db.execSQL(sqlCrear);
+
+    }
+
+}
--- app/src/main/java/com/seguridapp/sdlg/login2/MainActivity.java
+++ app/src/main/java/com/seguridapp/sdlg/login2/MainActivity.java
@@ -2,6 +2,8 @@ package com.seguridapp.sdlg.login2;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +13,10 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     Usuarios usuarios = new Usuarios();
+    BDSistema bdSistema;
+    String Consulta;
+    SQLiteDatabase bd;
+    Cursor c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +26,59 @@ public class MainActivity extends AppCompatActivity {
         EditText pss = findViewById(R.id.etPassword);
         usr.setText("");
         pss.setText("");
+        bdSistema = new BDSistema(this,"BDSistema",null,1);
+        bd = bdSistema.getWritableDatabase();
+        IniciarBD();
+
+
     }
 
+    void IniciarBD()
+    {
+        //Comprobando si la BD Abre correctamente
+        if (bd!=null)
+        {
+            //Generar valores
+            //int idUsuario= i;
 
+            String email = "oscar", password = "123";
+            //Creando cadena SQL
+            Consulta = "insert into tblUsuarios (Email,Password) ";
+            Consulta += "VALUES ('"+email+"' , '"+password+"')";
+            bd.execSQL(Consulta);
+
+            email = "alex";
+            //Creando cadena SQL
+            Consulta = "insert into tblUsuarios (Email,Password) ";
+            Consulta += "VALUES ('"+email+"' , '"+password+"')";
+            bd.execSQL(Consulta);
+
+            email = "fernando";
+            //Creando cadena SQL
+            Consulta = "insert into tblUsuarios (Email,Password) ";
+            Consulta += "VALUES ('"+email+"' , '"+password+"')";
+            bd.execSQL(Consulta);
+
+            //bd.close();
+        }
+        else{
+            AlertDialog.Builder ale = new AlertDialog.Builder(this);
+            ale.setMessage("No se pudieron iniciar los datos");
+            ale.setTitle("Error de Datos");
+            ale.setPositiveButton("Ok",new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    EditText usr = findViewById((R.id.etUsuario));
+                    EditText pss = findViewById(R.id.etPassword);
+                    dialog.cancel();
+                    usr.setText("");
+                    pss.setText("");
+                }
+            });
+            AlertDialog alerta = ale.create();
+            alerta.show();
+        }
+    }
 
     public void OnButtonClick(View v)
     {
@@ -30,6 +86,9 @@ public class MainActivity extends AppCompatActivity {
         EditText pss = findViewById(R.id.etPassword);
         String user = usr.getText().toString();
         String pass = pss.getText().toString();
+        String consulta = "select Email,Password from tblUsuarios where Email='"+user.trim()+"' and Password='"+pass.trim()+"'";
+        //else if(!c.moveToFirst())
+        c = bd.rawQuery(consulta,null);
         if (v.getId()==R.id.btIngresar)
         {
 
@@ -41,7 +100,8 @@ public class MainActivity extends AppCompatActivity {
             {
                 pss.setError("Campo vacío");
             }
-            else if(!usuarios.validarCorreo(user) || !usuarios.getPassword(user).equals(pass))
+            //else if(!usuarios.validarCorreo(user) || !usuarios.getPassword(user).equals(pass))
+            else if(!c.moveToFirst())
             {
                 AlertDialog.Builder ale = new AlertDialog.Builder(this);
                 ale.setMessage("Correo o contraseña inválidos");
